ec7c3e76d5d51d11078e7c95904188f3eb529cf4
Hyeonjoonn <phj3046@gmail.com>
2023-09-21 19:34:54 +0900

    init

---
+++ Targets/TuistProjectKit/Tests/TuistProjectKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistProjectKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistProjectKit", "TuistProjectKit")
+    }
+}
---
+++ Targets/TuistProjectUI/Tests/TuistProjectUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistProjectUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistProjectUI", "TuistProjectUI")
+    }
+}
